1f380152d6d26178314923e9ef3d2da38d876e94
ct <1528907295@qq.com>
2018-08-15 01:38:42 -0700

    add thread pool header file

---
+++ threadpool.h
@@ -0,0 +1,49 @@
+#ifndef _THREADPOOL_H
+#define _THREADPOOL_H 
+
+typedef struct threadpool_t threadpool_t;
+
+
+
+/*
+ *
+ *desc: create a thread pool 
+ */ 
+threadpool_t* threadpool_create(int min_thr_num,int max_thr_num,int queue_max_size);
+
+
+
+
+/*
+ *desc: add a new task to the queue of a thread pool 
+ *
+ */ 
+int threadpool_add(threadpool_t* pool,void*(function)(void* arg),void* arg);
+
+
+/*
+ *desc: destory a thread pool 
+ *
+ */ 
+int threadpool_destory(threadpool_t *pool);
+
+
+/*
+ *desc: work thread  
+ *
+ */ 
+void* threadpool_thread(void* threadpool);
+
+
+/*
+ *desc: manager thread 
+ *
+ *
+ */ 
+void* adjust_thread(void* threadpool);
+
+
+#endif
+
+
+
